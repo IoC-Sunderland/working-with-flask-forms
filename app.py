0048420ac69de5ea02a6dfde9d7d6dce9f47e4d8
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-11-19 09:16:01 +0000

    Added base template - 'public_template.html' and extended into 'sign_up.html'

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/sign-up")
+# Flask routes support GET requests by default. 
+# However it must be declared if the methods argument is provided.
+@app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     return render_template("sign_up.html")
 
