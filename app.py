446c69d4fbd8731b7def60fa808a16e1ab0e342b
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-11-19 09:33:29 +0000

    Added request and redirect

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 # Flask routes support GET requests by default. 
 # However it must be declared if the methods argument is provided.
 @app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+
+        return redirect(request.url)
+
     return render_template("sign_up.html")
 
 
