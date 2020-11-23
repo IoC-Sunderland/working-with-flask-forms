8397fae377a6ca492e44295471acf4d04cc95c6d
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-11-23 09:45:19 +0000

    Added data/test_database.db and code to connect and query test user

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
-from flask import Flask, render_template, request, redirect
+import sqlite3
+from flask import Flask, render_template, request, redirect, g
 app = Flask(__name__)
+
+# Path to sqlite3 database
+DATABASE = 'data/test_database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def index():
+    cur = get_db().cursor() #  Get the database cursor (this allows us to issue query statements)
+    select_all = cur.execute('SELECT * from users;')
+    rv = select_all.fetchall()
+    print('\n')
+    print(rv)
+    print('\n')
+    
+    return render_template("sign_up.html")
 
 # Flask routes support GET requests by default. 
 # However it must be declared if the methods argument is provided.
